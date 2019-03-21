d690a87c3c19a0637d218790db635009682a7656
Selena Solis <lenansolis@gmail.com>
2019-03-21 00:01:17 -0500

    add onclicks

--- MyPortfolio/src/stressReliever/stressReliever.js
+++ MyPortfolio/src/stressReliever/stressReliever.js
@@ -1,10 +1,19 @@
+
 function randomPoop(){
-    var square = Math.floor(Math.random()*24)+ 2;
+    var square = Math.floor(Math.random()*23)+2;
+    console.log("square " + square);
     if (square == 5 || square == 21){
         randomPoop();
     }
     else{
-        console.log(square);
         document.getElementById("sq" + square).style.display = "block";
+    }
+}
+
+function clickMe(n){
+    if (document.getElementById("sq" + n).style.display = "block"){
+        document.getElementById("sq" + n).style.display = "none";
+        document.getElementById("counter").innerHTML = parseFloat(document.getElementById("counter").innerHTML) + 1;
+        randomPoop();
     }
 }
