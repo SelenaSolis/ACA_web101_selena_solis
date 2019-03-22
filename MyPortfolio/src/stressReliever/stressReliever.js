27d7d1b10cd926cf32d87d7a2d980ed1cc417d81
Selena Solis <lenansolis@gmail.com>
2019-03-21 19:30:27 -0500

    add fart sound

--- MyPortfolio/src/stressReliever/stressReliever.js
+++ MyPortfolio/src/stressReliever/stressReliever.js
@@ -6,6 +6,13 @@ function randomPoop(){
         randomPoop();
     }
     else{
+        var audio = document.getElementById("sound");
+        if (audio.paused) {
+            audio.play();
+        }
+        else {
+            audio.currentTime = 0
+        }
         document.getElementById("sq" + square).style.display = "block";
     }
     startButton();
