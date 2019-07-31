82f02290db85aa46bc59da7e870790e147b079c7
Selena Solis <lenansolis@gmail.com>
2019-07-30 23:49:36 -0500

    update portfolio

---
+++ MyPortfolio/src/acaStore/README.md
@@ -0,0 +1,82 @@
+# Intro
+The purpose of this project is to build an Amazon.com like ecommerce website using the front end skills that we should possess by now. You may be suprised that you have already learned all the skills necessary to do this.
+# Prerequisite
+* Know how to loop arrays and use array functions like map.
+* Know how to construct strings, concatenation or string template literals.
+* Know how to learn about the use of built in tools such as innerHTML or sessionStorage. (read instruction manual)
+
+# Setup
+* Fork, Clone, Create a pull request
+* As you complete each feature of the app; add, commit and push so that we can track your progress
+* Simply open the index.html file in a chrome browser window. Double click on the file or use the menu -> file -> open file
+* Refresh the browser whenever you make changes to see if they worked.
+
+
+
+
+# Instructions
+
+## Products
+* Look at products.js. Here we have an array of hard coded product objects which is put into a global variable `products`. Feel free to change this data as you see fit.
+
+
+## UI
+* All UI decisions are up to you. You can make it as styled or unstyled as you want.
+* Its a good idea to use string template literals to dynamically construct html strings.
+* There should probably be one function responsible for creating each dynamic UI content.
+### Products
+* Our site is very simple, we only have a few hard coded products and we display all the products on the front page. Decide how you would like to display the products in your web page. Use `<div> <ul>` or `<ol>`. Show the product name, rating, number of reviews and price. Include a link or button that should be clicked on so that you can see the details of the product. Our store is very simple, we just show every product there is by default
+### Search
+* Create a textbox that is going to be used to type in a product to search for. Put a button next to it to initiate the search.
+### Shopping Cart
+* Create a button somwhere on the page "View Cart", that when clicked will show the products in the user's cart. This will be another list of products, but this one should only show the products that have been added to the cart. Add a button for each product for "remove".
+* There should be button for checkout
+### Product Detail
+* Decide how to show one single product. Display all the information that is on a product object. There should be a button for "Add to Cart". Add a `<select>` for quantity. Hard code the options from 1 to 10.
+### Category
+* Put a `<select>` somewhere on the page that will list out all the different product categories. The default option should be "--Choose--".
+### Checkout
+* Create a Form with inputs for First Name, Last Name and Email Address. Also have a submit button that says "Place Order"
+* Show the total price of the order.
+
+
+
+
+## Functionality
+* Write all your code in index.js. If you want to seperate the code out even more, feel free to add more .js files as you see fit.
+
+### List Out Products
+* Use the array of products to dynamically create the html however you see fit. The map function might be useful for this. Think about using loops and appendChild or innerHtml. 
+### Search
+* When the user clicks on the search button, retrieve the text that is in the textbox and use it to filter the array of products on name or description. Rerun the code that generates the html so that the UI updates to only show the filtered products.
+### View Details
+* When the user clicks on the view details link or button; clear the existing page content. Build the html needed to show the details of one product on the page.
+### Are You Still There
+* Keep track of the user's activity. Create a global variable to keep track of whether the user has interacted with the webpage in the last minute. The variable will be true or false. Set it to true.
+* Register an onclick event handler on the html body which will change this variable to true every time anything on the page is clicked.
+* Research how to use setInterval. Check the variable every minute to test whether it is true or false. If it is false, call `alert("Are you still there?");` and then change it to true. If it is true, change it to false.
+### Shopping Cart
+* Research sessionStorage. This is where we are going to store the data for the cart so that it will stay put even after the page is refreshed.
+* Research JSON.parse and JSON.stringify
+* sessionStorage only saves strings so we need to convert from an array to a string and visa versa
+* Add an item to sessionStorage called "cart", it should be an array
+### Add to Cart
+* When add to cart is clicked from the product detail, retrieve the cart item from the sessionStorage. Convert it from a string to an array. Push the product object to this array. Convert the array back to a string and save it back into sessionStorage
+### View Cart
+* When the View Cart button is clicked, clear the screen and construct a list of the products that are in the cart. Also show a button "Checkout" that when clicked will show the check out form.
+### Category
+* When the category drop down is changed, filter the array of products on the category property and update the UI to show this.
+### Checkout
+* When the Checkout button is clicked change the UI to show the check out form.
+
+## Debug  
+* Make sure you know how to debug with console.log
+* Ensure functions are being called by using console.log to see if the code was executed
+
+## Questions
+* Why is storing the shopping cart in sessionStorage not the best choice?
+* What should happen when the Place Order button is clicked?
+
+
+## Server
+* https://acastore.herokuapp.com/
---
+++ MyPortfolio/src/acaStore/public/index2.js
@@ -0,0 +1,243 @@
+let products = [];
+let loggedIn;
+if(loggedIn != 'undefined'){
+    loggedIn = true;
+}
+
+function loggedInView(){
+    let user = JSON.parse(localStorage.getItem('user'));
+    let welcomeDiv = document.getElementById('welcome');
+    let signUpDiv = document.getElementById('signUp');
+    let name = user.email.split('@')[0];
+    name = name.toUpperCase()
+    welcomeDiv.innerHTML = `Hello, ${name}`
+    signUpDiv.style.display = 'none';
+}
+
+
+const asyncLocalStorage = {
+    setItem: async function (key, value) {
+        await null;
+        return localStorage.setItem(key, value);
+    },
+    getItem: async function (key) {
+        await null;
+        return localStorage.getItem(key);
+    }
+};
+
+function getUserFetch(email){
+    fetch("https://acastore.herokuapp.com/users")
+        .then(response => response.json())
+        .then(data => user = data.find(user => user.email == email))
+        .then(user => {
+            asyncLocalStorage.setItem("user", JSON.stringify(user))
+            .then(function(){
+                return asyncLocalStorage.getItem('user');
+            })
+        })
+}
+
+function getProductsFetch(){
+    fetch("https://acastore.herokuapp.com/products")
+    .then(response => response.json())
+    .then(data => products = data)
+    .then(products => listProducts(products))
+}
+
+window.onload = function(){
+    getProductsFetch()
+    if(localStorage.getItem('user') && loggedIn == true){
+        loggedInView(); 
+    }
+}
+
+function listProducts(products){
+    let productsDiv = document.getElementById("products");
+    let cartDiv = document.getElementById("cart");
+    let newProd = '';
+    //maps through products and creates a div for each product
+    products.map(product =>{
+        newProd += `<div id = 'product'>
+                <a onclick = 'moreInfo(${product.id})'><div><img src=${product.imgUrl}></div>
+                <div><h4 id ='${product.id}'>${product.name}</h4></a></div>
+                <div id='hiddenReviews ${product.id}' class='hiddenReviews'><br></div>
+                <div><h5>${product.description}</h5></div>
+                <div>${product.price}</div>
+                <div id='rating ${product.id}' onmouseover='displayReviews(${product.id})' onmouseout = 'hideReviews(${product.id})'>
+                    rating: ${product.rating}
+                </div>
+                <div><button onclick = 'addToCart(${product.id})' class='cartButton' id='cartButton${product.id}'>Add To Cart</button></div>
+                <div><button onclick = 'moreInfo(${product.id})' id = 'moreInfo'>More Information</button></div>
+                <div id = 'moreInfo${product.id}'></div>
+            </div>`;
+    }) 
+    productsDiv.innerHTML = newProd + '<div id="subtotal"></div>'
+
+    if(localStorage.getItem("user") != null){
+        userStorage = JSON.parse(localStorage.getItem("user"))
+        if(userStorage.cartId != null){
+            getCartFetch(userStorage.id)
+            let cartStorage = JSON.parse(localStorage.getItem("cart"));
+            let itemsCounter = 0;
+            cartStorage.products.map(p => itemsCounter = itemsCounter + Number(p.quantity));
+            cartDiv.innerHTML = itemsCounter
+        }
+        
+    }
+    else{
+        cartDiv.innerHTML = 0;
+    }
+}
+
+function filterCategory(cat){
+    //if all is selected, displays all products
+    if (cat === 'all'){
+        listProducts(products);
+        return products;
+    }
+    //filters products depending on category selected
+    else{
+        let filteredProducts = products.filter(prod => 
+            prod.category === cat);
+        listProducts(filteredProducts);
+        return filteredProducts;
+    }
+}
+
+function searchFunc(){
+    let category = document.getElementById('categories').value;
+    
+    //assigns search text to variable
+    let searchText = document.getElementById("searchText").value;
+    searchText.toLowerCase();
+    //shows only products matching search
+    let filteredProducts = filterCategory(category).filter(prod => 
+        prod.name.toLowerCase().includes(searchText) || prod.description.toLowerCase().includes(searchText));
+    //lists only filtered products
+    listProducts(filteredProducts)
+}
+
+function logIn(){
+    let txtEmail = document.getElementById("email");
+    let txtPassword = document.getElementById("password");
+    
+    fetch("https://acastore.herokuapp.com/users")
+        .then(response => response.json())
+        .then(data => user = data.find(user => user.email === txtEmail.value))
+        .then(user => {
+            if(user.password == txtPassword.value){
+                asyncLocalStorage.setItem("user", JSON.stringify(user))
+                    .then(function(){
+                        loggedInView();
+                    })
+                if(user.cartId){
+                    getCartFetch(user.cartId)
+                }
+            }
+            else{
+                alert("try again");
+            }
+        })
+    if(localStorage.getItem('user')){
+        return loggedIn = true;
+    }
+}
+
+class User {
+    constructor(email, password, cartId) {
+      this.email = email;
+      this.password = password;
+      this.cartId = cartId;
+    }
+}
+
+function signUp(){
+    let txtPassword = document.getElementById("password");
+    let txtEmail = document.getElementById("email");
+    
+    fetch("https://acastore.herokuapp.com/users")
+        .then(response => response.json())
+        .then(data => user = data.find(user => user.email == txtEmail.value))
+        .then(user => {
+            if(user){
+                alert("that email address already has an account")
+            }
+            else{
+                let newUser = new User(txtEmail.value, txtPassword.value, null);
+                fetch("https://acastore.herokuapp.com/users", {
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify(newUser)
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        asyncLocalStorage.setItem("user", JSON.stringify(data))
+                            .then(function(){
+                                loggedInView();
+                                listProducts(products);
+                            })
+                    })
+                return loggedIn = true;
+            }
+        })
+}
+
+function moreInfo(prodId){
+    //assigns empty string  
+    let moreInfoDiv = '';
+    //filters array to only the product requested
+    let viewProd = products.filter(prod =>prod.id === prodId)
+    listProducts(viewProd);
+    //allows access to the moreInfo div to display all reviews and ratings
+    let productDiv = document.getElementById(`moreInfo${prodId}`);
+    viewProd[0].reviews.map(review =>
+        moreInfoDiv += `<div class = 'review'><div>${review.description}</div>
+            <div>${review.rating}</div>
+            </div>`
+        )
+    productDiv.innerHTML = moreInfoDiv;
+}
+
+function displayReviews(prodId){
+    product = products.find(p => p.id === prodId);
+    let reviewDiv = document.getElementById(`hiddenReviews ${prodId}`);
+    let newRev = '';
+    for(i=0; i<3; i++){
+        if(product.reviews[i]){
+            newRev += `<div class = 'review'><div>${product.reviews[i].description}</div>
+            <div>${product.reviews[i].rating}</div>
+            </div>`
+        }
+    }
+    reviewDiv.innerHTML = newRev;
+}
+
+function hideReviews(prodId){
+    let reviewDiv = document.getElementById(`hiddenReviews ${prodId}`);
+    let newRev = `<br>`;
+    reviewDiv.innerHTML = newRev;
+}
+
+function submitFunc(){
+    alert("We have received your order!");
+}
+
+//function called when customer clicks checkout button
+//displays form for data entry
+function checkoutForm(){
+    let productsDiv = document.getElementById("products");
+    let form = `<form >
+            First name:<br>
+            <input type="text" name="firstname">
+            <br>
+            Last name:<br>
+            <input type="text" name="lastname">
+            <br>
+            Email:<br>
+            <input type="text" name="email">
+            <br>
+            <input type="submit" value="Place Order" onclick='submitFunc()'>
+        </form> `
+    productsDiv.innerHTML = form;
+}
